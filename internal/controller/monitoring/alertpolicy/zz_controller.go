45349cd370e9c7becefedc334ff5348bb50c9546
Hasan Turken <turkenh@gmail.com>
2022-01-07 23:18:02 +0300

    Bump monitoring group to v1alpha2
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- internal/controller/monitoring/alertpolicy/zz_controller.go
+++ internal/controller/monitoring/alertpolicy/zz_controller.go
@@ -34,14 +34,14 @@ import (
 	tjcontroller "github.com/crossplane/terrajet/pkg/controller"
 	"github.com/crossplane/terrajet/pkg/terraform"
 
-	v1alpha1 "github.com/crossplane-contrib/provider-jet-gcp/apis/monitoring/v1alpha1"
+	v1alpha2 "github.com/crossplane-contrib/provider-jet-gcp/apis/monitoring/v1alpha2"
 )
 
 // Setup adds a controller that reconciles AlertPolicy managed resources.
 func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, s terraform.SetupFn, ws *terraform.WorkspaceStore, cfg *tjconfig.Provider, concurrency int) error {
-	name := managed.ControllerName(v1alpha1.AlertPolicy_GroupVersionKind.String())
+	name := managed.ControllerName(v1alpha2.AlertPolicy_GroupVersionKind.String())
 	r := managed.NewReconciler(mgr,
-		xpresource.ManagedKind(v1alpha1.AlertPolicy_GroupVersionKind),
+		xpresource.ManagedKind(v1alpha2.AlertPolicy_GroupVersionKind),
 		managed.WithExternalConnecter(tjcontroller.NewConnector(mgr.GetClient(), ws, s, cfg.Resources["google_monitoring_alert_policy"])),
 		managed.WithLogger(l.WithValues("controller", name)),
 		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))),
@@ -53,6 +53,6 @@ func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, s terra
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
 		WithOptions(controller.Options{RateLimiter: rl, MaxConcurrentReconciles: concurrency}).
-		For(&v1alpha1.AlertPolicy{}).
+		For(&v1alpha2.AlertPolicy{}).
 		Complete(r)
 }
